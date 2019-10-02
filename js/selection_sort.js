0d2c6ed2d75db998c35cec2f33448c5c864c6308
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-10-02 23:22:27 +0900

    added sort shuffle button and graph shuffling function

--- js/selection_sort.js
+++ js/selection_sort.js
@@ -8,30 +8,43 @@
 	const form = document.querySelector(".jsForm");
 	const input = document.querySelector("#jsInput");
 	const clrBtn = document.querySelector("#jsClearBtn");
+	const sortBtn = document.querySelector("#jsSortBtn");
+	const shuffleBtn = document.querySelector("#jsShuffleBtn");
+
+	const widthRatio = 0.7;
+	const heightRatio = 0.7;
+	const barWidthRatio = 0.3;
 
 	let list = [];
 
-	class Rect {
+	const shuffleList = function(list) {
+		for (let i = 1; i < list.length; ++i) {
+			const randomIdx = Math.floor(Math.random() * (i+1));
+
+			const tmp = list[i];
+			list[i] = list[randomIdx];
+			list[randomIdx] = tmp;
+		}
 	};
+	const sortList = function(list) {
+	};
+
 
 	const clearCanvas = function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	};
-
-	const drawGraph = function(list, widthRatio, heightRatio, barWidthRatio) {
+	const drawGraph = function(list) {
 		clearCanvas();
-
 		const barNum = list.length;
 		const graphWidth = (canvas.width * widthRatio) / barNum;
 		const barWidth = graphWidth * barWidthRatio;
 		const graphHeight = canvas.height * heightRatio;
-
 		const leftBottom = [ canvas.width * ((1 - widthRatio)/2),
 							canvas.height * ((1 + heightRatio)/2) ];
 
 		ctx.fillStyle = 'black';
 		for (let i=0; i<barNum; ++i) {
-			ctx.fillRect(leftBottom[0] + (graphWidth*i), leftBottom[1] - (graphHeight*((i+1)/barNum)),
+			ctx.fillRect(leftBottom[0] + (graphWidth*i), leftBottom[1] - (graphHeight*((list[i]+1)/barNum)),
 							barWidth, graphHeight*((list[i]+1)/barNum));
 		}
 	};
@@ -51,23 +64,24 @@
 		list = tmpList;
 
 		clearCanvas();
-		drawGraph(list, 0.7, 0.7, 0.3);
+		drawGraph(list);
 	};
 
-	const shuffleList = function(list) {
-		for (let i = 1; i < list.length; ++i) {
-			const randomIdx = Math.floor(Math.random() * (i+1));
-
-			const tmp = list[i];
-			list[i] = list[randomIdx];
-			list[randomIdx] = tmp;
-		}
+	const sortCanvas = function(event) {
+		sortList(list);
+		drawGraph(list);
+	};
+	const shuffleCanvas = function(event) {
+		shuffleList(list);
+		drawGraph(list)
 	};
 
 	// main
 	(function() {
-		if (form)	{ form.addEventListener("submit", initList); }
-		if (clrBtn)	{ clrBtn.addEventListener("click", clearCanvas); }
+		if (form)		{ form.addEventListener("submit", initList); }
+		if (clrBtn)		{ clrBtn.addEventListener("click", clearCanvas); }
+		if (sortBtn)	{ sortBtn.addEventListener("click", sortCanvas); }
+		if (shuffleBtn)	{ shuffleBtn.addEventListener("click", shuffleCanvas); }
 
 	})();
 })();
